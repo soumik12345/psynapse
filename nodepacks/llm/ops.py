d6a930bb1471571d48de8cf6c424ef2fac28fd54
soumik12345 <19soumik.rakshit96@gmail.com>
2025-11-09 21:14:01 +0530

    update: nodepacks

--- nodepacks/llm/ops.py
+++ nodepacks/llm/ops.py
@@ -11,14 +11,14 @@ def LLM_Message(
 
 
 def create_openai_reponse(
-    model: str, messages: list[dict[str, Any | dict[str, Any]]]
+    model: str, messages: str | dict[str, Any] | list[dict[str, Any | dict[str, Any]]]
 ) -> dict[str, Any | dict[str, Any]]:
     """
     Create an OpenAI response.
 
     Args:
         model: The model to use
-        messages: The messages to send to the model
+        messages: The messages to send to the model, can be a string or a list of LLM messages
 
     Returns:
         The response from the model
@@ -32,6 +32,7 @@ def create_openai_reponse(
     if not api_key_present:
         raise ValueError("OPENAI_API_KEY is not set in the environment")
     client = OpenAI()
+    messages = [messages] if isinstance(messages, dict) else messages
     response = client.responses.create(
         model=model, input=messages, stream=False
     ).model_dump()
